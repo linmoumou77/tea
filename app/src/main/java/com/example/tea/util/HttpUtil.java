f1f501974f2797f51f5cf28997197aaa06d7c4c5
linmoumou77 <44640174+linmoumou77@users.noreply.github.com>
2020-04-16 20:50:18 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/tea/util/HttpUtil.java
@@ -0,0 +1,15 @@
+package com.example.tea.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    //发起HTTP请求，传入请求地址，注册一个回调来处理服务器响应
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
