adfb569c36503c56f3f40cc4809edcc05387d75c
linmoumou77 <44640174+linmoumou77@users.noreply.github.com>
2020-04-17 18:41:35 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/tea/gson/AQI.java
+++ app/src/main/java/com/example/tea/gson/AQI.java
@@ -1,4 +1,12 @@
 package com.example.tea.gson;
 
 public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String api;
+        public String pm25;
+    }
+
 }
--- app/src/main/java/com/example/tea/gson/Basic.java
+++ app/src/main/java/com/example/tea/gson/Basic.java
@@ -1,4 +1,20 @@
 package com.example.tea.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+
 }
